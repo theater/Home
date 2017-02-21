d372b15c333abbf3a2055d7ca7b9eccea3133457
Konstantin Polihronov <polychronov@gmail.com>
2017-02-21 21:09:30 +0200

    Should be fine. To be tested with hardware.
    Initial logic is done using interrupt for the trigger and timer for off.
    
    Signed-off-by: Konstantin Polihronov <polychronov@gmail.com>

--- Arduino/BathRoomMirrorHeatingCtrl/BathRoomMirrorHeatingCtrl.cpp
+++ Arduino/BathRoomMirrorHeatingCtrl/BathRoomMirrorHeatingCtrl.cpp
@@ -19,27 +19,6 @@ void relayOff() {
 	digitalWrite(OUTPUT_PIN_RELAY, LOW);
 }
 
-//LOCAL void ICACHE_FLASH_ATTR read_input_pin(void *arg)
-//{
-//	uint32 gpio_status;
-//	ETS_GPIO_INTR_DISABLE(); // Disable gpio interrupts
-//	sleepms(15);
-//	if(!GPIO_INPUT_GET(PIN_GPIO13)) {
-//	button_counter++;
-//	console_printf("READ_INPUT_PIN executed...");
-//	console_printf("COUNTER:%d\n",button_counter);
-//	os_timer_disarm(&input_pin_timer); // Disarm input pin timer
-//	os_timer_setfn(&input_pin_timer, (os_timer_func_t *)handle_pin_press, NULL); // Setup input pin timer
-//	os_timer_arm(&input_pin_timer, button_timeout, 1); // Arm input pin timer, 0,5sec, repeat
-//	sleepms(button_debounce);  // debounce time
-//	}
-//	gpio_status = GPIO_REG_READ(GPIO_STATUS_ADDRESS);
-//	GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, gpio_status); 	//clear interrupt status
-//
-//	ETS_GPIO_INTR_ENABLE(); // Enable gpio interrupts}
-//}
-
-
 void pinReadDebounced() {
 	detachInterrupt(INPUT_PIN_IR_TRIGGER);
 	delay(15);
@@ -58,7 +37,7 @@ void pinReadDebounced() {
 
 //The setup function is called once at startup of the sketch
 void setup() {
-	pinMode(INPUT_PIN_IR_TRIGGER, INPUT_PULLUP);
+	pinMode(INPUT_PIN_IR_TRIGGER, INPUT);
 	attachInterrupt(digitalPinToInterrupt(INPUT_PIN_IR_TRIGGER), pinReadDebounced, RISING);
 	pinMode(OUTPUT_PIN_RELAY, OUTPUT);
 }
